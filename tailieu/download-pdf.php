5de7c006c573fd35ac82a493082d91b9f7a0627a
LAPTOP\Admin <haphan2k2@gmail.com>
2023-11-03 20:05:06 +0700

    File download-pdf.php to check before user download file

---
+++ tailieu/download-pdf.php
@@ -0,0 +1,16 @@
+﻿<?php
+if (isset($_GET['pdf'])) {
+    $pdfFilePath = $_GET['pdf'];
+
+    // Kiểm tra xem tệp PDF tồn tại
+    if (file_exists($pdfFilePath)) {
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="' . basename($pdfFilePath) . '"');
+        readfile($pdfFilePath);
+    } else {
+        echo 'Lỗi: Tệp PDF không tồn tại.';
+    }
+} else {
+    echo 'Lỗi: Không tìm thấy tệp PDF để tải về.';
+}
+?>
