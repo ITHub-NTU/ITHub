493877dd70613a5989c45ef8e1dbeb9a5a8cada7
Neul Hien <91861926+neul-hien@users.noreply.github.com>
2023-11-03 18:07:47 +0700

    Update ThaoLuanBV.php
    Create funtion baoCaoThaoLuanBaiViet(), layDanhSachThaoLuanBiCaoCao(), layThaoLuanBiBaoCao(), layNguoiDungBaoCao(), xoaThaoLuanViPham(), boQuaThaoLuanBiBaoCao()

--- class/ThaoLuanBV.php
+++ class/ThaoLuanBV.php
@@ -46,5 +46,111 @@
 				return $result;	
 			}
 		}
+		public function baoCaoThaoLuanBaiViet(){
+			if($this->maTLBV && $this->maLoaiVP && $this->taiKhoan){
+				$sqlQuery = "
+					INSERT IGNORE INTO `tbltlbvvipham` (`maTLBV`, `maLoaiVP`, `taiKhoan`) 
+					VALUES ('".$this->maTLBV."', '". $this->maLoaiVP."', '". $this->taiKhoan."');
+				";
+				$stmt = $this->conn->prepare($sqlQuery);
+				if ($stmt->execute()) {
+					return true;
+				}
+				return false;
+			}
+		}
+		public function layDanhSachThaoLuanBiCaoCao(){
+			if($this->maCDKD){
+				$sqlQuery = "
+							SELECT
+							TLBV.maTLBV,
+							TLBV.maBV,
+							TLBV.taiKhoan,
+							TLBV.noiDungTLBV,
+							LV.tenLoaiVP,
+							TLBV.ngayDangTLBV,
+							COUNT(TLBVVP.maTLBV) AS SoLuongBaoCao
+						FROM TblThaoLuanBV TLBV
+						LEFT JOIN TblTLBVViPham TLBVVP ON TLBV.maTLBV = TLBVVP.maTLBV
+						INNER JOIN TblLoaiViPham LV ON TLBVVP.maLoaiVP = LV.maLoaiVP
+						LEFT JOIN TblBaiViet BV ON TLBV.maBV = BV.maBV
+						WHERE TLBV.maBV IN (
+							SELECT maBV
+							FROM TblBaiViet
+							WHERE maCD = '".$this->maCDKD."'
+						)
+						GROUP BY TLBV.maTLBV
+						ORDER BY SoLuongBaoCao DESC;
+				";			
+				$stmt = $this->conn->prepare($sqlQuery);
+				$stmt->execute();
+				$result = $stmt->get_result();			
+				return $result;	
+			}
+		}
+		public function layThaoLuanBiBaoCao(){
+			if($this->maTLBVBC){
+				$sqlQuery = "
+								SELECT * FROM tblthaoluanbv  
+								WHERE maTLBV = '".$this->maTLBVBC."'
+				";
+				$stmt = $this->conn->prepare($sqlQuery);
+				$stmt->execute();
+				$result = $stmt->get_result();			
+				return $result;
+			}
+		}
+		public function layNguoiDungBaoCao(){
+			if($this->maTLBVBC){
+				$query = "SELECT bvvp.taiKhoan, GROUP_CONCAT(lvp.tenLoaiVP) AS danhSachLoaiVP
+				FROM tbltlbvvipham bvvp
+				INNER JOIN tblloaivipham lvp ON bvvp.maLoaiVP = lvp.maLoaiVP
+				WHERE bvvp.maTLBV = '".$this->maTLBVBC."'
+				GROUP BY bvvp.taiKhoan";
+	
+				$stmt = $this->conn->prepare($query);
+				$stmt->execute();
+				$result = $stmt->get_result();
+				return $result;
+			}
+		}	
+		public function xoaThaoLuanViPham(){
+			if ($this->maTLBVBC) {
+				$this->conn->begin_transaction();
+		
+				try {
+					// Xóa báo cáo thảo luận vi phạm
+					$queryDeleteReports = "DELETE FROM tbltlbvvipham WHERE maTLBV = ?";
+					$stmtReports = $this->conn->prepare($queryDeleteReports);
+					$stmtReports->bind_param('s', $this->maTLBVBC);
+					$stmtReports->execute();
+		
+					// Xóa thảo luận vi phạm
+					$queryDeleteTLBV = "DELETE FROM tblthaoluanbv WHERE maTLBV = ?";
+					$stmtTLBV = $this->conn->prepare($queryDeleteTLBV);
+					$stmtTLBV->bind_param('s', $this->maTLBVBC);
+					$stmtTLBV->execute();
+					
+					$this->conn->commit();
+		
+					return true;
+				} catch (Exception $e) {
+					$this->conn->rollback();
+					return false;
+				}
+			}
+		}
+		public function boQuaThaoLuanBiBaoCao(){
+			if($this->maTLBVBC){
+				$queryDeleteReports = "DELETE FROM tbltlbvvipham WHERE maTLBV = ?";
+				$stmt = $this->conn->prepare($queryDeleteReports);
+				$stmt->bind_param('s', $this->maTLBVBC);
+		
+				if ($stmt->execute()) {
+					return true;
+				}
+				return false;
+			}
+		}
 	}
 ?>
