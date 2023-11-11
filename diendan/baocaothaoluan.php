10bfc4ee4ee6a1fede5604a0ed21e2c46179734c
Bee <130392282+KietBee@users.noreply.github.com>
2023-11-11 21:17:40 +0700

    Update baocaothaoluan.php Add Notification

--- diendan/baocaothaoluan.php
+++ diendan/baocaothaoluan.php
@@ -2,10 +2,14 @@
     include_once '../config/Database.php';
     include_once '../class/ThaoLuanBV.php';
     include_once '../class/TienIch.php';
+    include_once '../class/BaiViet.php';
+    include_once '../class/ThongBao.php';
     $database = new Database();
     $db = $database->getConnection();
     $tienIch = new TienIch();
     $tblThaoLuanBV = new ThaoLuanBV($db);
+    $tblBaiViet = new BaiViet($db);
+    $tblThongBao = new ThongBao($db);
     if(isset($_GET['maTLBV'])&&isset($_GET['maLoaiVP'])){
         $maBV = $_GET['maBV'];
         $maTLBV = $_GET['maTLBV'];
@@ -14,8 +18,13 @@
         $tblThaoLuanBV->maTLBV = $maTLBV;
         $tblThaoLuanBV->maLoaiVP = $maLoaiVP;
         $tblThaoLuanBV->taiKhoan = $taiKhoan;
+        $tblBaiViet->maBV = $maBV;
+        $tblBaiViet->maLoaiVP = $maLoaiVP;
+        $tblBaiViet->taiKhoan = $taiKhoan;
+        $chiTietBaiViet = $tblBaiViet->layBaiViet();
         if($tblThaoLuanBV->baoCaoThaoLuanBaiViet()){
             header("location: chitietbaiviet.php?maBV=$maBV&msgtl=1");
+            $tblThongBao->themTBBV('','baocaothaoluan', $chiTietBaiViet['maCD'], $maBV);
         }
         else{
             header("location: chitietbaiviet.php?maBV=$maBV&msgtl=0");
