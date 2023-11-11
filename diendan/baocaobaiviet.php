01a7da1da4cdc79923ef72181c7fd02ca27f5ef1
Bee <130392282+KietBee@users.noreply.github.com>
2023-11-11 20:46:40 +0700

    Update baocaobaiviet.php Add Notification

--- diendan/baocaobaiviet.php
+++ diendan/baocaobaiviet.php
@@ -2,20 +2,23 @@
     include_once '../config/Database.php';
     include_once '../class/BaiViet.php';
     include_once '../class/TienIch.php';
-    //-------------------------------
+    include_once '../class/ThongBao.php';
     $database = new Database();
     $db = $database->getConnection();
     $tienIch = new TienIch();
     $tblBaiViet = new BaiViet($db);
-    //-------------------------------
+    $tblThongBao = new ThongBao($db);
     if(isset($_GET['maBV'])&&isset($_GET['maLoaiVP'])){
         $maBV = $_GET['maBV'];
         $maLoaiVP = $_GET['maLoaiVP'];
         $taiKhoan = $_GET['taiKhoan'];
-        //-------------------------------
+        $tblBaiViet->maBV = $maBV;
+        $tblBaiViet->maLoaiVP = $maLoaiVP;
+        $tblBaiViet->taiKhoan = $taiKhoan;
+        $chiTietBaiViet = $tblBaiViet->layBaiViet();
         if($tblBaiViet->baoCaoBaiViet()){
             header("location: chitietbaiviet.php?maBV=$maBV&msg=1");
-            //-------------------------------
+            $tblThongBao->themTBBV('','baocao', $chiTietBaiViet['maCD'], $maBV);
         }
         else{
             header("location: chitietbaiviet.php?maBV=$maBV&msg=0");
